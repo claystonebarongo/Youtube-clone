233ccbb7dc09ef5a3a262cd6d43b9d13f8a02092
Clay <claystonebarongo@gmail.com>
2026-02-12 13:25:41 +0300

    playVideo updates

--- src/components/PlayVideo/PlayVideo.jsx
+++ src/components/PlayVideo/PlayVideo.jsx
@@ -13,6 +13,104 @@ const PlayVideo = () => {
         <div className='play-video'>
 
             <video src={video1} controls autoPlay muted></video>
+            <h3>Best Youtube channel to learn web development</h3>
+            <div className="play-video-info">
+                <p>125 Views &bull; 2 days ago</p>
+
+
+                <div>
+                    <span><img src={like} alt="" />125</span>
+                    <span><img src={dislike} alt="" />2</span>
+                    <span><img src={share} alt="" />Share</span>
+                    <span><img src={save} alt="" />Save</span>
+                </div>
+            </div>
+
+            <hr />
+            <div className="publisher">
+                <img src={jack} alt="" />
+                <div>
+                    <p>C.B mogonchi</p>
+                    <span>1M Subscribers</span>
+                </div>
+                <button>Subscribers</button>
+            </div>
+
+            <div className="vide-description">
+                <p>Channel that makes learning Easy</p>
+                <p>Subscribe to C.B mogonchi to watch More tutorials on Web Development</p>
+                <hr />
+                <h4>130 comments</h4>
+                <div className="comment">
+                    <img src={user_profile} alt="" />
+                    <div>
+                        <h3>Judith Muli <span>1 day ago</span></h3>
+                        <p>Dread It or love it ,Destiny arrives all the same</p>
+                        <div className="comment-action">
+                            <img src={like} alt="" />
+                            <span>244</span>
+                            <img src={dislike} alt="" />
+                        </div>
+                    </div>
+
+                </div>
+
+
+                <div className="comment">
+                    <img src={user_profile} alt="" />
+                    <div>
+                        <h3>Judith Muli <span>1 day ago</span></h3>
+                        <p>Dread It or love it ,Destiny arrives all the same</p>
+                        <div className="comment-action">
+                            <img src={like} alt="" />
+                            <span>244</span>
+                            <img src={dislike} alt="" />
+                        </div>
+                    </div>
+
+                </div>
+                <div className="comment">
+                    <img src={user_profile} alt="" />
+                    <div>
+                        <h3>Judith Muli <span>1 day ago</span></h3>
+                        <p>Dread It or love it ,Destiny arrives all the same</p>
+                        <div className="comment-action">
+                            <img src={like} alt="" />
+                            <span>244</span>
+                            <img src={dislike} alt="" />
+                        </div>
+                    </div>
+
+                </div>
+                <div className="comment">
+                    <img src={user_profile} alt="" />
+                    <div>
+                        <h3>Judith Muli <span>1 day ago</span></h3>
+                        <p>Dread It or love it ,Destiny arrives all the same</p>
+                        <div className="comment-action">
+                            <img src={like} alt="" />
+                            <span>244</span>
+                            <img src={dislike} alt="" />
+                        </div>
+                    </div>
+
+                </div>
+                <div className="comment">
+                    <img src={user_profile} alt="" />
+                    <div>
+                        <h3>Judith Muli <span>1 day ago</span></h3>
+                        <p>Dread It or love it ,Destiny arrives all the same</p>
+                        <div className="comment-action">
+                            <img src={like} alt="" />
+                            <span>244</span>
+                            <img src={dislike} alt="" />
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+
 
         </div>
     )
