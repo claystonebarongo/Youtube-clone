19f083f79c89c3038e4f10a1bcbc38a22e2b938e
Clay <claystonebarongo@gmail.com>
2026-02-14 22:08:25 +0300

    daily updates

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -9,6 +9,7 @@ const Home = ({ sidebar }) => {
   return (
     <>
       <Sidebar sidebar={sidebar} />
+
       <div className={`container ${sidebar ? "" : 'large-container'}`}>
 
         <Feed />
